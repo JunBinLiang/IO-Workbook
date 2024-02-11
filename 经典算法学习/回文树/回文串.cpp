4862e4bc4d8d5148cf92af5473ea270c5670de68
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-02-10 23:17:21 -0500

    Update 回文串.cpp

--- 经典算法学习/回文树/回文串.cpp
+++ 经典算法学习/回文树/回文串.cpp
@@ -39,56 +39,71 @@ ostream& operator <<(ostream& out, const vector<T>& a) {
   return out;
 }
 
-//len[i]代表的是i号节点代表的回文串长度
 
-const int N = 4e5 + 100;
 
-char s[N];
-int len[N], fail[N], ch[N][26], cnt[N];
-int tot = 0, last = 0;
 
-int getfail(int x, int i) { //寻找fail node
-  while(s[i - len[x] - 1] != s[i]) x = fail[x];
-  return x;
-}
+const int N = 5e5 + 100;
+struct PalindromeTree {
+    char s[N];
+    int len[N], fail[N], ch[N][26], cnt[N];
+    int tot = 0, last = 0;    
+    
+    void init() {
+        memset(cnt, 0, sizeof cnt);
+        s[0] = -1;
+        len[0] = 0; //初始化even root 
+        len[1] = -1;//初始化odd root 
+        fail[0] = 1;
+        fail[1] = 1;
+        tot = 1;
+        last = 0;
+    }
+    
+    int getfail(int x, int i) { //寻找fail node
+        while(i - len[x] - 1 < 0 || s[i - len[x] - 1] != s[i]) x = fail[x];
+        return x;
+    }
+    
+    int newnode(int x) { //简历长度为x 的新node
+        tot++;
+        len[tot] = x;
+        return tot;
+    }
 
-int newnode(int x) { //简历长度为x 的新node
-  tot++;
-  len[tot] = x;
-  return tot;
-}
+    
+    void add(int i) {
+        s[i] -= 'a';
+        int failnode = getfail(last, i);
+        if(ch[failnode][s[i]] == 0) { //需要新建node
+            int node = newnode(len[failnode] + 2); //前后都加上新字符，所以新回文串长度要加2
+            fail[node] = ch[getfail(fail[failnode], i)][s[i]]; //因为fail指向的是原串的严格后缀，所以要从failnode的fail开始找起 
+            ch[failnode][s[i]] = node;
+        }
+        last = ch[failnode][s[i]];
+        cnt[last]++;
+    }
+} pt1;
+
+
+
 
 void solve() {
-    scanf("%s", s + 1);
-    int n = strlen(s + 1);
+    scanf("%s", pt1.s + 1);
+    int n = strlen(pt1.s + 1);
     
-    memset(cnt, 0, sizeof cnt);
-    s[0] = -1;
-    len[0] = 0; //初始化even root 
-    len[1] = -1;//初始化odd root 
-    fail[0] = 1;
-    tot = 1;
-    last = 0;
-    
+    pt1.init();
     for(int i = 1; i <= n; i++) {
-      s[i] -= 'a';
-      int failnode = getfail(last, i);
-      if(ch[failnode][s[i]] == 0) { //需要新建node
-        int node = newnode(len[failnode] + 2); //前后都加上新字符，所以新回文串长度要加2
-        fail[node] = ch[getfail(fail[failnode], i)][s[i]]; //因为fail指向的是原串的严格后缀，所以要从failnode的fail开始找起 
-        ch[failnode][s[i]] = node;
-      }
-      last = ch[failnode][s[i]];
-      cnt[last]++;
+        pt1.add(i);
     }
     
     ll ans = 0;
-    for(int i = tot; i >= 2; i--) {
-      ans = max(ans, (cnt[i] + 0ll) * len[i]);
-      cnt[fail[i]] += cnt[i];
+    for(int i = pt1.tot; i >= 2; i--) {
+      ans = max(ans, (pt1.cnt[i] + 0ll) * pt1.len[i]);
+      pt1.cnt[pt1.fail[i]] += pt1.cnt[i];
     }
 
     cout << ans << endl;
+
 }
 
 int main()
