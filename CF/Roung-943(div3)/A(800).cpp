1f0dac01a78cef70e6dfc94ebd12bc59615b5fd7
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-05-16 04:21:23 -0400

    Create A(800).cpp

---
+++ CF/Roung-943(div3)/A(800).cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <string.h>
+
+using namespace std;  
+using ll = long long;
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define ve vector
+
+/*void kill() {
+  vector<int> a;
+  for(int i = 0; i < 1000000; i++) a.push_back(i);
+}*/
+
+void solve() {
+  int x;
+  cin >> x;
+  cout << x - 1 << endl;
+
+}
+
+int main(){
+  int t = 1;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
