7308dae0be2eb3034f49da457e515af31a7688c3
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-06-04 00:38:30 -0400

    Create A(800).cpp

---
+++ CF/Round-907(div2)/A(800).cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <string.h>
+#include <random>
+#include <chrono>
+
+using namespace std;  
+using ll = long long;
+using ull = unsigned long long;
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for(int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  set<int> s = {1, 2, 4, 8, 16};
+  string ans = "YES";
+  for(int i = 1; i < n; i++) {
+    if(s.find(i) != s.end()) continue;
+    if(a[i] < a[i - 1]) {
+      ans = "NO";
+    }
+  }
+  cout << ans << endl;
+}
+
+int main(){
+  int t = 1;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
