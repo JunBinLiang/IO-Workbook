2216092e7ce50f5a5091678536ff87d1871ef9f2
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-06-16 04:14:30 -0400

    Create CF-1977D(2300).cpp

---
+++ 经典算法学习/Xor-Hash/CF-1977D(2300).cpp
@@ -0,0 +1,67 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+ 
+mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
+ 
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<bool>> table(n, vector<bool>(m));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            char c;
+            cin >> c;
+            table[i][j] = c - '0';
+        }
+    }
+    vector<long long> rands(n), rands2(n);
+    for (int i = 0; i < n; ++i) {
+        rands[i] = rnd();
+        rands2[i] = rnd();
+    }
+    map<pair<long long, long long>, int> ans;
+    int res = 0;
+    pair<int, int> ind_ans = {0, 0};
+    for (int j = 0; j < m; ++j) {
+        long long summ = 0, summ2 = 0;
+        for (int i = 0; i < n; ++i) {
+            if (table[i][j]) {
+                summ ^= rands[i];
+                summ2 ^= rands2[i];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            summ ^= rands[i];
+            summ2 ^= rands2[i];
+            ans[{summ, summ2}]++;
+            if (res < ans[{summ, summ2}]) {
+                res = ans[{summ, summ2}];
+                ind_ans = {j, i};
+            }
+            summ ^= rands[i];
+            summ2 ^= rands2[i];
+        }
+    }
+    cout << res << "\n";
+    string inds(n, '0');
+    for (int i = 0; i < n; ++i) {
+        if (table[i][ind_ans.first]) {
+            if (i != ind_ans.second) {
+                inds[i] = '1';
+            }
+        } else if (ind_ans.second == i) {
+            inds[i] = '1';
+        }
+    }
+    cout << inds << "\n";
+}
+
+int main(){
+  int t = 1;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
