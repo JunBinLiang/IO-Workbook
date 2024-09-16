a3f3a53ae3163c5af36a3e979a5a64b76d4f3e3c
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-09-16 01:03:08 -0400

    Create A(800).cpp

---
+++ CF/Round-971(div4)/A(800).cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<unordered_set>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<string>
+#include <string.h>
+#include <bitset>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    cout << b - a << endl;
+}     
+
+int main() {
+    int t = 1;
+    std :: cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
